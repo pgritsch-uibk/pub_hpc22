17d1af70710210d25b5a0c7b96f6345dc75b1c7b
GagaLP <GabrielMitterrutzner@gmail.com>
2022-11-17 22:31:52 +0100

    added working cmake for 1D_Stencil but porgramm doesn't work

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -4,15 +4,15 @@
 
 for i in 1 2
 do
-    sbatch --output=output_philipp_hs_parallel_256_$i.log job_parallel.sh 
+    sbatch --output=output_parallel_$i.log job_parallel.sh 
 done
 
-for i in 1 2
-do
-    sbatch --output=output_philipp_hs_parallel_single_256_$i.log job_parallel_single.sh 
-done
+# for i in 1 2
+# do
+#     sbatch --output=output_single_$i.log job_parallel_single.sh 
+# done
 
-for i in 1 2
-do
-    sbatch --output=output_philipp_hs_sequential_256_$i.log job_sequential.sh 
-done
+# for i in 1 2
+# do
+#     sbatch --output=output_sequential_$i.log job_sequential.sh 
+# done
