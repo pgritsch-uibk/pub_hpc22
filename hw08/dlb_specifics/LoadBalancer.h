fe484dad850494db74e0fc90151f9e721d5a18d4
Philipp Gritsch <p.gritsch@student.uibk.ac.at>
2022-12-11 13:24:44 +0100

    fixes implementation + 2 lines chunks

--- hw08/dlb_specifics/LoadBalancer.h
+++ hw08/dlb_specifics/LoadBalancer.h
@@ -9,7 +9,7 @@ class LoadBalancer {
 	int sharedNextY{};
 	int myRank{};
 	int onRank{};
-	const int one = 1;
+	const int two = 2;
 
   public:
 	LoadBalancer(int onRank) {
@@ -25,11 +25,11 @@ class LoadBalancer {
 
 	~LoadBalancer() { MPI_Win_free(&sharedNextYWindow); }
 
-	int getNextY() {
+	int getNextTwoY() {
 		int nextY;
 
 		MPI_Win_lock(MPI_LOCK_SHARED, onRank, 0, sharedNextYWindow);
-		MPI_Fetch_and_op(&one, &nextY, MPI_INT, onRank, 0, MPI_SUM, sharedNextYWindow);
+		MPI_Fetch_and_op(&two, &nextY, MPI_INT, onRank, 0, MPI_SUM, sharedNextYWindow);
 		MPI_Win_unlock(onRank, sharedNextYWindow);
 
 		return nextY;
